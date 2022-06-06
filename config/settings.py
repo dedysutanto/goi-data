c01ecfd550c5b85efc1c606e5a9403d3ecb91716
gkiserpong <gkiserpongit@gmail.com>
2022-06-06 15:51:20 +0700

    Bug with python-dotenv and wsgi

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
