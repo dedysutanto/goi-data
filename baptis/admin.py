1c4e8281d14cf69ce1a7b41bff2f6d4ff5e0856e
gkiserpong <gkiserpongit@gmail.com>
2022-06-06 22:57:33 +0700

    Rearrange baptis entry

--- baptis/admin.py
+++ baptis/admin.py
@@ -3,9 +3,25 @@ from .models import Baptis
 
 
 class BaptisAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'classes': ('',),
+            'fields': ('parokia', 'baptis_date', 'number')
+        }),
+        (None, {
+            'classes': ('',),
+            'fields': ('member', 'baptis_klerus', 'baptis_parent')
+        }),
+        (None, {
+            'classes': ('',),
+            'fields': ('baptis_name', 'baptis_anniversary')
+        }),
+    )
 
     list_display = ['number', 'member', 'baptis_parent', 'baptis_klerus', 'baptis_name', 'baptis_date', 'baptis_anniversary']
     search_fields = ('number', 'member__name', 'baptis_parent__name', 'baptis_klerus__name')
+    ordering = ['number']
+    list_per_page = 25
 
     class Meta:
         model = Baptis
