565f981a6fd4bd8dac47e71e6bb02318e417e502
gkiserpong <gkiserpongit@gmail.com>
2022-06-07 00:32:22 +0700

    Parokia di baptis

--- baptis/admin.py
+++ baptis/admin.py
@@ -19,7 +19,7 @@ class BaptisAdmin(admin.ModelAdmin):
     )
 
     list_display = ['number', 'baptis_date', 'member', 'baptis_name',
-                    'baptis_parent', 'baptis_klerus', 'baptis_anniversary']
+                    'baptis_parent', 'baptis_klerus', 'baptis_anniversary', 'parokia']
     search_fields = ('number', 'member__name', 'member__baptis_name',
                      'baptis_parent__name', 'baptis_klerus__name__name', 'baptis_klerus__name__baptis_name')
     ordering = ['number']
