243e23f09fa72cb2dd23a6118da5696d23f5aabd
gkiserpong <gkiserpongit@gmail.com>
2022-06-13 12:58:02 +0700

    Additonal information in map

--- landing/views.py
+++ landing/views.py
@@ -7,9 +7,6 @@ def index(request):
     parokia = Parokia.objects.all()
     komox = Komox.objects.all()
 
-    print(settings.GOOGLE_MAPS_API_KEY)
-    print(settings.MAPS_CENTER)
-
     return render(request, 'index.html', {
         'parokia': parokia,
         'komox': komox,
