47961ac2812c89daa21611877f5859d6b091dd9b
gkiserpong <gkiserpongit@gmail.com>
2022-06-13 11:52:58 +0700

    print settings

--- landing/views.py
+++ landing/views.py
@@ -7,6 +7,10 @@ def index(request):
     parokia = Parokia.objects.all()
     komox = Komox.objects.all()
 
+    print(settings.GOOGLE_MAPS_API_KEY)
+    print(settings.MAPS_CENTER)
+    print(settings.MAPS_ZOOM)
+
     return render(request, 'index.html', {
         'parokia': parokia,
         'komox': komox,
